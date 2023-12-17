05ff5fad543d520442aaeae5fe560bc88af613c6
Akim <akim.malyschik@gmail.com>
2023-12-17 16:49:18 +0100

    remove python-dotenv

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,4 @@
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-SERVER_IP = os.getenv("SERVER_IP", "192.168.0.100")
+SERVER_IP = "192.168.0.100"
 SERVER_LOCAL_IP = "0.0.0.0"
 PORT = 5001
 BUFFER_SIZE = 4096
